566a2a964aa546a4de94c6894799f5769193a014
Aakash Dhar <aakash.dhar@simteq.com>
2018-04-11 08:48:04 +0530

    Initial Changes

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,9 +4,9 @@
   <!-- Logo -->
   <a href="/" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>E</b>MS</span>
+    <span class="logo-mini"><b>C</b>MS</span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>Siimteq</b> EMS</span>
+    <span class="logo-lg"><b>Siimteq</b> CMS</span>
   </a>
 
   <!-- Header Navbar -->
